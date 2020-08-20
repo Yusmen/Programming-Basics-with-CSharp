52203896d7935475f1fdcf45a0881050cf55c3b8
Yusmen <mojito@abv.bg>
2020-08-21 01:54:35 +0300

    Adding SimpleCalculations Exercise Tasks

---
+++ SimpleCalculationsExercise/Birthday/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            double p = double.Parse(Console.ReadLine());
+            double obem = l * d * h;
+            double litri = obem * 0.001;
+            p = p * 0.01;
+            litri = litri * (1 - p);
+            Console.WriteLine("{0:F3}",litri);
+
+        }
+    }
+}
---
+++ SimpleCalculationsExercise/Farenhait/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farenhait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double g = double.Parse(Console.ReadLine());
+            double f = g * 1.8 + 32;
+            Math.Round(f, 2);
+            Console.WriteLine(f);
+
+        }
+    }
+}
